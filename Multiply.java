565790f71cee73c800f6f9003977117011a7efb8
Amar Pathak <amarpathak723@gmail.com>
2023-07-26 19:26:40 +0530

    Adding Multiply.java

---
+++ Multiply.java
@@ -0,0 +1,9 @@
+public class Multiply {
+    public static void main(String[] args) {
+        
+    
+     int a = 19;
+        int b = 9;
+        System.out.println("ths product is : "+ (a*b));
+    }
+}
