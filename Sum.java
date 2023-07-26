74ae030297b02a04bb344e7a570607c346efb1d3
Amar Pathak <amarpathak723@gmail.com>
2023-07-26 19:15:08 +0530

    Adding message in Sum.java

--- Sum.java
+++ Sum.java
@@ -2,7 +2,7 @@ public class Sum{
     public static void main(String[] args) {
         int a = 8;
         int b = 9;
-        System.out.println(a+b);
+        System.out.println("ths sum is : "+ (a+b));
 
     }
 }
